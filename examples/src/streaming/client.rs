cd1ee3e320ffeb4f6a1e6ed19edfd8389cdb16d9
blakezyli <blakezyli@tencent.com>
2023-02-17 10:27:46 +0800

    Change example for streaming test

--- examples/src/streaming/client.rs
+++ examples/src/streaming/client.rs
@@ -2,10 +2,15 @@ pub mod pb {
     tonic::include_proto!("grpc.examples.echo");
 }
 
+use futures::future::FutureExt;
+use futures::future::{BoxFuture, Fuse};
 use futures::stream::Stream;
 use std::time::Duration;
+use tokio::io::AsyncReadExt;
+use tokio::sync::mpsc;
 use tokio_stream::StreamExt;
 use tonic::transport::Channel;
+use tracing_subscriber::filter::FilterExt;
 
 use pb::{echo_client::EchoClient, EchoRequest};
 
@@ -33,18 +38,48 @@ async fn streaming_echo(client: &mut EchoClient<Channel>, num: usize) {
 }
 
 async fn bidirectional_streaming_echo(client: &mut EchoClient<Channel>, num: usize) {
-    let in_stream = echo_requests_iter().take(num);
+    let (sx, rx) = mpsc::channel(16);
 
     let response = client
-        .bidirectional_streaming_echo(in_stream)
+        .bidirectional_streaming_echo(tokio_stream::wrappers::ReceiverStream::new(rx))
         .await
         .unwrap();
 
     let mut resp_stream = response.into_inner();
 
-    while let Some(received) = resp_stream.next().await {
-        let received = received.unwrap();
-        println!("\treceived message: `{}`", received.message);
+    let send_future = async {
+        loop {
+            let res = tokio::io::stdin().read_i64().await;
+            match res {
+                Ok(i) => {
+                    println!("input {}", i);
+                    sx.send(EchoRequest {
+                        message: format!("msg {:02}", i),
+                    }).await;
+                }
+                Err(e) => println!("input error {}", e),
+            };
+        }
+    };
+    let send_future = send_future.fuse();
+    let send_future = FutureExt::boxed(send_future);
+
+    let recv_future = async {
+        while let Some(received) = resp_stream.next().await {
+            let received = received.unwrap();
+            println!("\treceived message: `{}`", received.message);
+        }
+    };
+    let recv_future = recv_future.fuse();
+    let recv_future = FutureExt::boxed(recv_future);
+
+    tokio::select! {
+        res = recv_future => {
+            println!("grpc recv completed");
+        },
+        res = send_future => {
+            println!("grpc send completed");
+        }
     }
 }
 
@@ -66,7 +101,7 @@ async fn bidirectional_streaming_echo_throttle(client: &mut EchoClient<Channel>,
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut client = EchoClient::connect("http://[::1]:50051").await.unwrap();
+    let mut client = EchoClient::connect("http://127.0.0.1:50052").await.unwrap();
 
     println!("Streaming echo:");
     streaming_echo(&mut client, 5).await;
--- examples/src/streaming/server.rs
+++ examples/src/streaming/server.rs
@@ -147,7 +147,7 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let server = EchoServer {};
     Server::builder()
         .add_service(pb::echo_server::EchoServer::new(server))
-        .serve("[::1]:50051".to_socket_addrs().unwrap().next().unwrap())
+        .serve("127.0.0.1:50052".to_socket_addrs().unwrap().next().unwrap())
         .await
         .unwrap();
 
